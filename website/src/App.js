62438ba9a2d78866a9336ffb0cf870a39d14d694
StudyNPaste <139663796+StudyNPaste@users.noreply.github.com>
2024-06-01 21:25:15 -0700

    hero complete

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Navbar } from './components/Navbar/Navbar';
+import { Reservation } from './components/Reservation/Reservation';
 
 const App = () => {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <Reservation/>
+    </div>
   )
 }
+
 
 export default App;
--- website/src/components/Reservation/Reservation.jsx
+++ website/src/components/Reservation/Reservation.jsx
@@ -1,7 +1,20 @@
-import './Reseravtion.css'
+import './Reservation.css'
+import hero from '../Assets/hero.jpg'
 
 export const Reservation = () => {
   return (
-    <div>Reservation</div>
+    <div className='hero'>
+        <div className='left-hero'>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+                We are a family owned Mediterranean restaurant, located on Maldore Street in Chicago, Illinois. We focus on traditional recipes served with a modern twist.
+            </p>
+            <button>Reserve a table</button>
+        </div>
+        <div className='right-hero'>
+            <img src={ hero } alt="" />
+        </div>
+    </div>
   )
 }
